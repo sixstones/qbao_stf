dd162163ea7a74c1b3e2e27ce7c727797bd6ab2f
JT-QL-10036 <jt-ql-10036@JT-QL-10036deiMac.local>
2017-11-09 15:13:10 +0800

    Initial commit

---
+++ res/auth/mock/scripts/signin/signin-controller.js
@@ -0,0 +1,42 @@
+module.exports = function SignInCtrl($scope, $http) {
+
+  $scope.error = null
+
+  $scope.submit = function() {
+    var data = {
+      name: $scope.signin.username.$modelValue,
+      email: $scope.signin.email.$modelValue,
+      passwd: $scope.signin.passwd.$modelValue
+    }
+    $scope.invalid = false
+    $http.post('/auth/api/v1/mock', data)
+      .success(function(response) {
+        $scope.error = null
+        location.replace(response.redirect)
+      })
+      .error(function(response) {
+        switch (response.error) {
+          case 'ValidationError':
+            $scope.error = {
+              $invalid: true
+            }
+            break
+          case 'QbaoError':
+            $scope.error = {
+              $authvalid: true
+            }
+            break
+          case 'InvalidCredentialsError':
+            $scope.error = {
+              $incorrect: true
+            }
+            break
+          default:
+            $scope.error = {
+              $server: true
+            }
+            break
+        }
+      })
+  }
+}
